a6a8b2e90cebcd486a3a0f909b2a7e6a4fe95630
danHamash <megadestroier@hotmail.com>
2025-01-27 22:32:36 -0300

    ajuste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shoping-list'`, () => {
+  it(`should have as title 'shopping-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shoping-list');
+    expect(app.title).toEqual('shopping-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shoping-list');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shopping-list');
   });
 });
